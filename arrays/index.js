728f716abe0b2c309d133f7543bea662791ce76b
Don Kimani <roykimani4@gmail.com>
2026-02-23 22:57:26 +0300

    utility functions

--- arrays/index.js
+++ arrays/index.js
@@ -24,6 +24,13 @@ for (let x=0; x<arr.length; x++){
 }
 // console.log(`total is: ${s}`);
 
+// reducer
+// const initial_value = 0;
+// const reducer = (accumulator, value) => {
+//     console.log(`accumulator is: ${accumulator}   value is ${value}`);
+//     return accumulator + value;
+// }
+// const total = arr.reduce(reducer, 0);
 
 //  Create utility functions (chunk, flatten, unique, groupBy)
 // chunk
@@ -34,4 +41,18 @@ const chunk = (arr1, chunksize) => {
     }
     return tempArray;
 }
-console.log(chunk(arr, 2))
+// console.log(chunk(arr, 2))
+
+// flattenarray
+const arr2 = [1,2,[3,4],[5,6]];
+function flattenArray(data, initialValue = []){
+
+    return data.reduce((acc, value) => {
+        Array.isArray(value) ? flattenArray(value, acc) : acc.push(value);
+        return acc;
+    }, initialValue);
+}
+// console.log(flattenArray(arr2))
+
+
+// compose functions
